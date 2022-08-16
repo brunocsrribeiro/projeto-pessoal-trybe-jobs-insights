034c76ead735015e7616c8837aba75f3fc0413a3
Bruno Ribeiro <ribeiro.brunocsr@gmail.com>
2022-08-16 17:24:29 -0300

    FEAT: Implemented test to ensure that the function correctly returns the number of occurrences of the requested word.

--- tests/counter/test_counter.py
+++ tests/counter/test_counter.py
@@ -1,5 +1,9 @@
-# from src.counter import count_ocurrences
+from src.counter import count_ocurrences
 
 
 def test_counter():
-    pass
+    report_JS = count_ocurrences("src/jobs.csv", "Javascript")
+    report_PY = count_ocurrences("src/jobs.csv", "Python")
+
+    assert report_JS == 122
+    assert report_PY == 1639
